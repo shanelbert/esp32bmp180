ab18583f5e5eef756ef867838cf3f703fc0b937e
shanelbert <shanelbert@yahoo.com>
2020-04-12 22:21:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const pool = new Pool({
 });
 
 express()
+  .use(bodyParser.json())
   .get('/', (req, res) => res.sendFile(path.join(__dirname + '/pages/index.html'), {headers: {'Content-Type': 'text/html'}}))
   .get('/db', async (req, res) => {
     try {
@@ -43,6 +44,19 @@ express()
     res.send(content);
     client.release();
 
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .post('/dbpost', (req, res) => {
+    try {
+      const client = await pool.connect()
+      
+      const nbelement = client.query('SELECT COUNT(*) FROM readings;')
+
+      client.query(`INSERT INTO readings VALUES (${nbelement+1}, ${req.body.sensor}, ${req.body.location}, ${parseFloat(req.body.temperature)}, ${parseFloat(req.body.altitude)}, ${parseFloat(req.body.pressure)}, ${req.body.timestamp});`)
+      client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);

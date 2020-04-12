7178311c41348c70f500d54fd1cbe0b04f7fb26e
shanelbert <57350807+shanelbert@users.noreply.github.com>
2020-04-12 16:18:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-Console.log("Hello World");
+const express = require('express')
+
+
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.render('index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

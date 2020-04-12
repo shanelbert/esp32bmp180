b9b6645e69b644467a90726f684900567072de80
shanelbert <shanelbert@yahoo.com>
2020-04-12 17:26:20 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
-  //~ .use(express.static(path.join(__dirname, 'pages')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname + '/pages/index.html')))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM readings');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));

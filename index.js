b6522e76e4abed1b5ee789c84c56ebb54b49242c
shanelbert <shanelbert@yahoo.com>
2020-04-12 16:34:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render('index.html'))
+  .get('/', (req, res) => res.sendFile('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

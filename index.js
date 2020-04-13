e08b5260a1d6da27e3d8276950bcdc2ad11f9db6
shanelbert <shanelbert@yahoo.com>
2020-04-13 11:38:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,13 @@ express()
       const nbelement = client.query('SELECT COUNT(*) FROM readings;')
 
       // client.query(`INSERT INTO readings VALUES (${nbelement+1}, ${req.body.sensor}, ${req.body.location}, ${parseFloat(req.body.temperature)}, ${parseFloat(req.body.altitude)}, ${parseFloat(req.body.pressure)}, ${req.body.timestamp});`)
-      client.query(`INSERT INTO readings VALUES (${nbelement+1}, "BMP180", "Jakarta", 205.3, 101.1, 101.5, "2020-10-03 10:00:00");`)
+      client.query(`INSERT INTO readings VALUES (${nbelement+1}, "BMP180", "Jakarta", 205.3, 101.1, 101.5, "2020-10-03 10:00:00");`, (err, res) => {
+        try {
+          if (err) throw err;
+        } catch {
+          console.error("Can't store the data");
+        }
+      });
       res.sendStatus(200);
       client.release();
     } catch (err) {
